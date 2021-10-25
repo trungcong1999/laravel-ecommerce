b8626e8a8cf0ddbce4edfd4049d767036344525f
Hoàng Trung Công <37680352+trungcong1999@users.noreply.github.com>
2021-10-25 21:39:54 +0700

    add
    adđ

---
+++ database/seeds/AdminsTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class AdminsTableSeeder extends Seeder
+{
+
+    /**
+     * Auto generated seed file
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+
+        DB::table('admins')->delete();
+        
+        DB::table('admins')->insert(array (
+            0 => 
+            array (
+                'username' => 'admin',
+                'name' => 'Electro',
+                'password' => '12345',
+                'created_at' => '2018-08-28',
+                'updated_at' => '2018-08-28',
+            ),
+        ));
+        
+        
+    }
+}
